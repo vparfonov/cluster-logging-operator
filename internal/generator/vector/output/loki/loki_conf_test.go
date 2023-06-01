c55421049396cb4e15adaa9747c32ddbbc63fca5
Vitalii Parfonov <vparfono@redhat.com>
2023-06-01 16:41:41 +0300

    Uncomment few test that was commented by mistake
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- internal/generator/vector/output/loki/loki_conf_test.go
+++ internal/generator/vector/output/loki/loki_conf_test.go
@@ -659,105 +659,106 @@ var _ = Describe("Generate vector config for in cluster loki", func() {
 	var f = func(clspec logging.CollectionSpec, secrets map[string]*corev1.Secret, clfspec logging.ClusterLogForwarderSpec, op generator.Options) []generator.Element {
 		return Conf(clfspec.Outputs[0], inputPipeline, secrets[constants.LogCollectorToken], generator.NoOptions)
 	}
-	DescribeTable("for Loki output", helpers.TestGenerateConfWith(f)) //		Entry("with default logcollector bearer token", helpers.ConfGenerateTest{
-	//			CLFSpec: logging.ClusterLogForwarderSpec{
-	//				Outputs: []logging.OutputSpec{
-	//					{
-	//						Type: logging.OutputTypeLoki,
-	//						Name: "loki-receiver",
-	//						URL:  "http://lokistack-dev-gateway-http.openshift-logging.svc:8080/api/logs/v1/application",
-	//					},
-	//				},
-	//			},
-	//			Secrets: map[string]*corev1.Secret{
-	//				constants.LogCollectorToken: {
-	//					Data: map[string][]byte{
-	//						"token": []byte("token-for-internal-loki"),
-	//					},
-	//				},
-	//			},
-	//			ExpectedConf: `
-	//[transforms.loki_receiver_remap]
-	//type = "remap"
-	//inputs = ["application"]
-	//source = '''
-	// del(.tag)
-	//'''
-	//
-	//[transforms.loki_receiver_dedot]
-	//type = "lua"
-	//inputs = ["loki_receiver_remap"]
-	//version = "2"
-	//hooks.init = "init"
-	//hooks.process = "process"
-	//source = '''
-	//   function init()
-	//       count = 0
-	//   end
-	//   function process(event, emit)
-	//       count = count + 1
-	//       event.log.openshift.sequence = count
-	//       if event.log.kubernetes == nil then
-	//           emit(event)
-	//           return
-	//       end
-	//       if event.log.kubernetes.labels == nil then
-	//           emit(event)
-	//           return
-	//       end
-	//		dedot(event.log.kubernetes.namespace_labels)
-	//       dedot(event.log.kubernetes.labels)
-	//       emit(event)
-	//   end
-	//
-	//   function dedot(map)
-	//       if map == nil then
-	//           return
-	//       end
-	//       local new_map = {}
-	//       local changed_keys = {}
-	//       for k, v in pairs(map) do
-	//           local dedotted = string.gsub(k, "[./]", "_")
-	//           if dedotted ~= k then
-	//               new_map[dedotted] = v
-	//               changed_keys[k] = true
-	//           end
-	//       end
-	//       for k in pairs(changed_keys) do
-	//           map[k] = nil
-	//       end
-	//       for k, v in pairs(new_map) do
-	//           map[k] = v
-	//       end
-	//   end
-	//'''
-	//
-	//[sinks.loki_receiver]
-	//type = "loki"
-	//inputs = ["loki_receiver_dedot"]
-	//endpoint = "http://lokistack-dev-gateway-http.openshift-logging.svc:8080/api/logs/v1/application"
-	//out_of_order_action = "accept"
-	//healthcheck.enabled = false
-	//
-	//[sinks.loki_receiver.encoding]
-	//codec = "json"
-	//
-	//[sinks.loki_receiver.labels]
-	//kubernetes_container_name = "{{kubernetes.container_name}}"
-	//kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
-	//kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
-	//kubernetes_pod_name = "{{kubernetes.pod_name}}"
-	//log_type = "{{log_type}}"
-	//
-	//[sinks.loki_receiver.tls]
-	//enabled = true
-	//ca_file = "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt"
-	//# Bearer Auth Config
-	//[sinks.loki_receiver.auth]
-	//strategy = "bearer"
-	//token = "token-for-internal-loki"
-	//`,
-	//		}),
+	DescribeTable("for Loki output", helpers.TestGenerateConfWith(f))
+	Entry("with default logcollector bearer token", helpers.ConfGenerateTest{
+		CLFSpec: logging.ClusterLogForwarderSpec{
+			Outputs: []logging.OutputSpec{
+				{
+					Type: logging.OutputTypeLoki,
+					Name: "loki-receiver",
+					URL:  "http://lokistack-dev-gateway-http.openshift-logging.svc:8080/api/logs/v1/application",
+				},
+			},
+		},
+		Secrets: map[string]*corev1.Secret{
+			constants.LogCollectorToken: {
+				Data: map[string][]byte{
+					"token": []byte("token-for-internal-loki"),
+				},
+			},
+		},
+		ExpectedConf: `
+	[transforms.loki_receiver_remap]
+	type = "remap"
+	inputs = ["application"]
+	source = '''
+	del(.tag)
+	'''
+	
+	[transforms.loki_receiver_dedot]
+	type = "lua"
+	inputs = ["loki_receiver_remap"]
+	version = "2"
+	hooks.init = "init"
+	hooks.process = "process"
+	source = '''
+	  function init()
+	      count = 0
+	  end
+	  function process(event, emit)
+	      count = count + 1
+	      event.log.openshift.sequence = count
+	      if event.log.kubernetes == nil then
+	          emit(event)
+	          return
+	      end
+	      if event.log.kubernetes.labels == nil then
+	          emit(event)
+	          return
+	      end
+			dedot(event.log.kubernetes.namespace_labels)
+	      dedot(event.log.kubernetes.labels)
+	      emit(event)
+	  end
+	
+	  function dedot(map)
+	      if map == nil then
+	          return
+	      end
+	      local new_map = {}
+	      local changed_keys = {}
+	      for k, v in pairs(map) do
+	          local dedotted = string.gsub(k, "[./]", "_")
+	          if dedotted ~= k then
+	              new_map[dedotted] = v
+	              changed_keys[k] = true
+	          end
+	      end
+	      for k in pairs(changed_keys) do
+	          map[k] = nil
+	      end
+	      for k, v in pairs(new_map) do
+	          map[k] = v
+	      end
+	  end
+	'''
+	
+	[sinks.loki_receiver]
+	type = "loki"
+	inputs = ["loki_receiver_dedot"]
+	endpoint = "http://lokistack-dev-gateway-http.openshift-logging.svc:8080/api/logs/v1/application"
+	out_of_order_action = "accept"
+	healthcheck.enabled = false
+	
+	[sinks.loki_receiver.encoding]
+	codec = "json"
+	
+	[sinks.loki_receiver.labels]
+	kubernetes_container_name = "{{kubernetes.container_name}}"
+	kubernetes_host = "${VECTOR_SELF_NODE_NAME}"
+	kubernetes_namespace_name = "{{kubernetes.namespace_name}}"
+	kubernetes_pod_name = "{{kubernetes.pod_name}}"
+	log_type = "{{log_type}}"
+	
+	[sinks.loki_receiver.tls]
+	enabled = true
+	ca_file = "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt"
+	# Bearer Auth Config
+	[sinks.loki_receiver.auth]
+	strategy = "bearer"
+	token = "token-for-internal-loki"
+	`,
+	})
 
 })
 
